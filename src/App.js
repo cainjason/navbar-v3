dfa76b39d1096db5d85526579a8dc8bcbde76fb5
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-12-05 02:06:36 -0500

    Chore: made house keeping changes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="app">
+      Nav practis
+      <Header />
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
